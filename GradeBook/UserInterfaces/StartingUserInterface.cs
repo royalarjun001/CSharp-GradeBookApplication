461691b3bb511d96f40424e6aaf1407f3682cf09
Arjun kumar verma <arjun.kumar.verma@jci.com>
2020-04-12 11:27:58 +0530

    Correction in logic

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -36,20 +36,23 @@ namespace GradeBook.UserInterfaces
             var parts = command.Split(' ');
             if (parts.Length != 3)
             {
-                Console.WriteLine("Command not valid, Create requires a name.");
+                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
             }
             var name = parts[1];
             var type = parts[2];
 
-            BaseGradeBook gradeBook = null;
+            BaseGradeBook gradeBook;
 
             if(type == "standard")
                 gradeBook = new StandardGradeBook(name);
             else if (type == "ranked")
                 gradeBook = new RankedGradeBook(name);
             else
-                Console.WriteLine(name + " is not a supported type of gradebook, please try again");
+            {
+                Console.WriteLine(type + " is not a supported type of gradebook, please try again");
+                return;
+            }
 
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
